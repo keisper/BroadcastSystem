19e1e10023a6e24aeb824ef13fd74923fa3eab5c
keisper <48211958+keisper@users.noreply.github.com>
2020-08-17 18:27:24 +0200

    ini

---
+++ client/build/precache-manifest.87ba30cfad68458fb9f558715042b1cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de59624c1e00c39853458e88e1e9023c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cad5af63995a6da638fa",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7cad48f253b409ae905",
+    "url": "/static/js/2.ddfd0f15.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.ddfd0f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad5af63995a6da638fa",
+    "url": "/static/js/main.66f58aed.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
